53baba1a6d8c50d535ced40b8d30f274cb8ddaff
sandy <sandydwi75@gmail.com>
2025-05-12 05:04:03 +0700

    feat: update docker config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const currentTime = DateTime.local();
 
   return (
-    <div className="w-full h-full min-h-screen max-h-screen flex flex-col items-center justify-center bg-linear-to-t from-sky-500 to-indigo-500 p-[25px]">
+    <div className="w-full h-full min-h-screen max-h-screen flex flex-col items-center justify-center bg-linear-to-bl from-violet-500 to-fuchsia-500">
       <div className="w-full h-auto px-[25px] py-[50px] bg-white rounded-[24px] max-w-[728px] flex flex-col items-center justify-center gap-[16px] shadow-lg border-[1px] border-gray-400">
         <p className="text-center text-[57px] leading-[64px] text-[#000000] font-semibold">
           {currentTime.toLocaleString(DateTime.TIME_WITH_SECONDS)}
