030c5229aba421549ccb1c24b60e1f2bfe0e6024
sandy <sandydwi75@gmail.com>
2025-05-13 02:19:07 +0700

    feat: update CSP config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,29 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'nonce-__REPLACE_NONCE__';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data:;
+              font-src 'self';
+              connect-src 'self';
+              frame-ancestors 'none';
+              object-src 'none';
+              base-uri 'self';
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
