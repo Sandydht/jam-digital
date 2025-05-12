a2c61a80820fe35f24afd0f90156169be0e64c2f
sandy <sandydwi75@gmail.com>
2025-05-13 03:12:06 +0700

    feat: update config

--- next.config.ts
+++ next.config.ts
@@ -2,29 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data:;
-              font-src 'self';
-              connect-src 'self';
-              frame-ancestors 'none';
-              object-src 'none';
-              base-uri 'self';
-            `.replace(/\s{2,}/g, ' ').trim(),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
